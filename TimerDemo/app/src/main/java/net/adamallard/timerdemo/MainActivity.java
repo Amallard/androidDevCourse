789a6a920fb55dcd5727410bb023b73da3d77284
amallard <adam.allard@live.com>
2016-11-12 14:18:32 -0800

    Adding Timer/Countdown demos

---
+++ TimerDemo/app/src/main/java/net/adamallard/timerdemo/MainActivity.java
@@ -0,0 +1,51 @@
+package net.adamallard.timerdemo;
+
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+
+        *********************
+        *                   *
+        *  Timer method #1  *
+        *                   *
+        *********************
+
+        final Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+
+                Log.i("Runnable executed", "Another second passed");
+
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.post(run);
+
+        */
+
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisecondsUntilDone) {
+
+                Log.i("Seconds left: ", String.valueOf(millisecondsUntilDone / 1000));
+            }
+
+            public void onFinish() {
+
+                Log.i("Done", "Countdown finished");
+            }
+        }.start();
+    }
+}
