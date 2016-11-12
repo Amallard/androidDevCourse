33281533a5a78cd6c4956637549479c18d25d650
amallard <adam.allard@live.com>
2016-11-12 14:03:09 -0800

    Adding ListView demo app

---
+++ ListViewDemo/app/src/main/java/net/adamallard/listviewdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package net.adamallard.listviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        final ArrayList<String> list = new ArrayList<String>();
+        list.add("Adam");
+        list.add("Kate");
+        list.add("Amara");
+        list.add("Silas");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Toast.makeText(getApplicationContext(), "Hello " + list.get(i), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
